6fc273f450da40a268ac2f5c52875fd24847740a
lauraygina <joyliss_baby_2211@hotmail.com>
2016-04-10 16:26:59 -0500

    jQuery primera clase

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*esconde el mensaje
+jQuery(".advertencia").hide();
+//mostrar el mensaje lentamente usamos atajo$
+$(".advertencia").show("slow");*/
+
+function enviarAdvertencia()
+{
+	jQuery(".advertencia").hide().show("slow");	
+}
+
+//le pasamos al handler la funcion sin parentesis
+$(document).ready(enviarAdvertencia);
