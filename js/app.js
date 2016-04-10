ee91421f4ee22d0583b534af8a7108049999743b
lauraygina <joyliss_baby_2211@hotmail.com>
2016-04-10 17:15:08 -0500

    ocultar un span en un boton

--- js/app.js
+++ js/app.js
@@ -3,10 +3,32 @@ jQuery(".advertencia").hide();
 //mostrar el mensaje lentamente usamos atajo$
 $(".advertencia").show("slow");*/
 
-function enviarAdvertencia()
+/*function enviarAdvertencia()
 {
 	jQuery(".advertencia").hide().show("slow");	
 }
 
+le pasamos al handler la funcion sin parentesis
+$(document).ready(enviarAdvertencia);*/
+
+//1. esconder spiler
+$(".Spoiler span").hide();
+
+//2. agregar boton
+$(".Spoiler").append("<button> Mostrar Spoiler! </button>");
+
+
+//3. cuando se presiona el boton
+//agregar un handler al evento click
+$(".Spoiler button").click(function(){
+	//3.1 Mostrar spoiler
+	$(".Spoiler span").show();
+	//3.2 Deshacerse del boton
+	$(this).remove();
+})
+function enviarAdvertencia()
+{
+	jQuery(".Spoiler").hide().show("slow");
+}
 //le pasamos al handler la funcion sin parentesis
 $(document).ready(enviarAdvertencia);
